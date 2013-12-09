703164234ba151f80b017bc2e8f688dfdc885c06
Tomas Alabes <tomas.alabes@gmail.com>
2013-12-09 12:40:11 -0300

    Jekyll v0

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,4 @@
-/*! TomasAlabes.me - v0.0.0 - 2013-12-08
+/*! TomasAlabes.me - v0.0.0 - 2013-12-09
 * http://tomasAlabes.me/
 * Copyright (c) 2013 Tomas Alabes; Licensed MIT */
 //Not using strict: uneven strict support in browsers, #392, and causes
