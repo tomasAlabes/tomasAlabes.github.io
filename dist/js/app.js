145b8f7260015ce5a0e77f1b9c32777dc58b9c4e
Tomas Alabes <tomas.alabes@gmail.com>
2014-01-02 15:58:24 -0300

    Organized images

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,4 @@
-/*! TomasAlabes.me - v0.0.0 - 2014-01-01
+/*! TomasAlabes.me - v0.0.0 - 2014-01-02
 * http://tomasAlabes.me/
 * Copyright (c) 2014 Tomas Alabes; Licensed MIT */
 //Not using strict: uneven strict support in browsers, #392, and causes
