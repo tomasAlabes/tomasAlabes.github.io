575fa19bd166c5dcd1f83b77ea700788a3bac6f7
Tomas Alabes <tomas.alabes@gmail.com>
2014-07-28 21:10:14 -0700

    Fixed Author link and post header perhaps

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,4 @@
-/*! TomasAlabes.me - v0.0.0 - 2014-07-27
+/*! TomasAlabes.me - v0.0.0 - 2014-07-28
 * http://tomasAlabes.me/
 * Copyright (c) 2014 Tomas Alabes; Licensed MIT */
 //Not using strict: uneven strict support in browsers, #392, and causes
